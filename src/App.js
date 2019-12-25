357b8738f6d318122faa54a57301b25ec847d4c6
kumkumsingh <kumkumsingh.r@gmail.com>
2019-12-25 11:34:33 +0100

    added constants file for url to get photos from unsplash API . able to fetch photos from unsplash API using superagent request.GET method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as request from "superagent";
+import { url }from './constants'
 
 function App() {
+  request
+  .get(url)
+  .then(photos => {
+    console.log("results",photos.body)
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      My Gallery
     </div>
   );
 }
